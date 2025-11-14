a6b7097cf86493f971bda6914e9621f33cc51e3a
falahsab <40568313+falahsab@users.noreply.github.com>
2025-11-14 23:01:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const API = "https://script.google.com/macros/s/AKfycbwalL1AfqLL2B3gOyrjz_FAKywJG7rbKdpT7wNQ8DEvinGT23VXPATS4CwcGjOhuFb9/exec";
+const API = "https://script.google.com/macros/s/AKfycbxRa_PLC_aO0EeWQBrNV7NXVoh6yb4ZlZrHz1o8oyhcu2QjXAac53Bjh3BKp8PIublL/exec";
+
+
+async function login() {
+let username = document.getElementById("username").value;
+let password = document.getElementById("password").value;
 
 
 let res = await fetch(API, {
@@ -38,60 +43,4 @@ document.getElementById("clientName").innerText = localStorage.getItem("client_n
 
 let res = await fetch(API, {
 method: "POST",
-body: JSON.stringify({
-action: "getClientData",
-client_id: localStorage.getItem("client_id")
-})
-});
-
-
-let data = await res.json();
-
-
-document.getElementById("total").innerText = data.total + " ريال";
-
-
-let html = `
-<tr><th>التاريخ</th><th>النوع</th><th>المبلغ</th><th>البيان</th></tr>
-`;
-
-
-data.list.forEach(r => {
-html += `<tr>
-<td>${new Date(r.date).toLocaleDateString()}</td>
-<td>${r.type === 'in' ? 'له' : 'عليه'}</td>
-<td>${r.amount}</td>
-<td>${r.note}</td>
-</tr>`;
-});
-
-
-document.getElementById("table").innerHTML = html;
-}
-
-
-// لوحة التحكم
-async function addTrans() {
-let client_id = document.getElementById("client_id").value;
-let amount = document.getElementById("amount").value;
-let type = document.getElementById("type").value;
-let note = document.getElementById("note").value;
-
-
-let res = await fetch(API, {
-method: "POST",
-body: JSON.stringify({
-action: "addTransaction",
-client_id,
-amount,
-type,
-note
-})
-});
-
-
-let data = await res.json();
-
-
-document.getElementById("msg").innerText = data.success ? "تمت الإضافة" : "فشل الإضافة";
 }
